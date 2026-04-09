53972d0a4d01ec4adfe0a652b09816f6bb578518
Brett Warminski <bwarminski@gmail.com>
2026-04-08 23:47:18 -0400

    refactor: remove redpanda collector scaffolding

---
+++ collector/test/compose_stack_test.rb
@@ -0,0 +1,22 @@
+# ABOUTME: Verifies the collector compose stack matches the supported local runtime.
+# ABOUTME: Guards the collector services against reintroducing dead Redpanda wiring.
+require "minitest/autorun"
+
+class ComposeStackTest < Minitest::Test
+  def test_collector_stack_no_longer_mentions_redpanda
+    compose = File.read(File.expand_path("../../docker-compose.yml", __dir__))
+
+    refute_includes compose, "redpanda:"
+    refute_includes compose, "REDPANDA_BROKERS"
+  end
+
+  def test_collector_image_no_longer_installs_redpanda_native_dependencies
+    dockerfile = File.read(File.expand_path("../Dockerfile", __dir__))
+
+    refute_includes dockerfile, "librdkafka-dev"
+  end
+
+  def test_redpanda_consumer_runtime_is_removed
+    refute File.exist?(File.expand_path("../lib/redpanda_consumer.rb", __dir__))
+  end
+end
