befe61968b263d67e3a04eb9626256febef9da63
Brett Warminski <bwarminski@gmail.com>
2026-04-09 23:50:32 -0400

    test: verify collector runtime against interval schema

--- collector/test/compose_stack_test.rb
+++ collector/test/compose_stack_test.rb
@@ -3,6 +3,24 @@
 require "minitest/autorun"
 
 class ComposeStackTest < Minitest::Test
+  def test_clickhouse_bootstrap_mentions_interval_and_state_tables
+    sql_files = Dir[File.expand_path("../db/clickhouse/*.sql", __dir__)].map { |path| File.basename(path) }
+
+    assert_includes sql_files, "002_collector_state.sql"
+    assert_includes sql_files, "003_query_intervals.sql"
+    assert_includes sql_files, "004_reset_query_analytics.sql"
+    refute_includes sql_files, "002_query_fingerprints.sql"
+    refute_includes sql_files, "003_top_offenders_mv.sql"
+    refute_includes sql_files, "004_reset_query_fingerprints.sql"
+  end
+
+  def test_compose_stack_schema_contract_mentions_collector_state_and_query_intervals
+    reset_sql = File.read(File.expand_path("../db/clickhouse/004_reset_query_analytics.sql", __dir__))
+
+    assert_includes reset_sql, "collector_state"
+    assert_includes reset_sql, "query_intervals"
+  end
+
   def test_collector_stack_no_longer_mentions_redpanda
     compose = File.read(File.expand_path("../../docker-compose.yml", __dir__))
 
